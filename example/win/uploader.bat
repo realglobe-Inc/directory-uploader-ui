1b344da6abbcb4056e25466593f8562abc88a898
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2016-11-02 16:51:55 +0900

    サンプルの変数値を呼び出し側から上書き可能にした

--- example/win/uploader.bat
+++ example/win/uploader.bat
@@ -1,23 +1,23 @@
 rem アップロード先サーバー
-set SERVER=http://13.78.122.199/rest
+if "%SERVER%"=="" set SERVER=http://13.78.122.199/rest
 rem サーバーに名乗る ID
-set ID=b03c5702-1cf1-48fd-b00b-bf8ec0577c2a
+if "%ID%"=="" set ID=b03c5702-1cf1-48fd-b00b-bf8ec0577c2a
 rem サーバーの認証トークン
-set TOKEN=a2fc62e5-4a5e-4aa9-a267-87d3c75a55ae
+if "%TOKEN%"=="" set TOKEN=a2fc62e5-4a5e-4aa9-a267-87d3c75a55ae
 rem サーバーで紐付くユーザー
-set USER=realglobe
+if "%USER%"=="" set USER=realglobe
 
 rem 監視するフォルダ
-set DIR=image
+if "%DIR%"=="" set DIR=image
 rem アップロードするファイルの種類
-set EXTENSION=jpg,jpeg,png,bmp,gif,tiff
+if "%EXTENSION%"=="" set EXTENSION=jpg,jpeg,png,bmp,gif,tiff
 rem アップロードするファイルの最小サイズ
-set MIN=1024
+if "%MIN%"=="" set MIN=1024
 rem アップロードするファイルの最大サイズ
-set MAX=8388608
+if "%MAX%"=="" set MAX=8388608
 
-set JAVA=java
-set JAR=directory-uploader-ui-1.3.0-jar-with-dependencies.jar
+if "%JAVA%"=="" set JAVA=java
+if "%JAR%"=="" set JAR=directory-uploader-ui-1.3.0-jar-with-dependencies.jar
 
 
 %JAVA% -jar %JAR% ^
